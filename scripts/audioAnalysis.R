16a8ce068f322bce38bde4f3262a93f3251d389e
mitchelkappen <36090493+mitchelkappen@users.noreply.github.com>
2022-05-18 16:00:45 +0200

    Getting everything together in one workable long format

--- scripts/audioAnalysis.R
+++ scripts/audioAnalysis.R
@@ -20,29 +20,56 @@ BASEPATH <- "D:/Data/EEG_Study_2/"
 plotPrefix <- paste0(BASEPATH, "Data/Interim/Audio/figures/")
 
 ##### Loading data ##### 
-
+# Audio Data
 audioData <-
   as.data.frame(read_parquet(paste0(BASEPATH,"Data/Raw/Audio/df_gemaps_func.parquet")))
 
+# Limesurvey Data
 questionData <- as.data.frame(read.csv("../loc_data/QuestionnaireResults.csv")) 
-questionData$participantNum = questionData$Participant.Number
+questionData$participantNum = questionData$Participant.Number # Change name for merge
 
-audioData = merge(audioData, questionData, by = "participantNum")
+# Behavioral Data
+behavioralData <-
+  as.data.frame(read_parquet(paste0(BASEPATH,"Data/Raw/Behavioral/behavioralData_CYBB_MIST.parquet")))
+behavioralData$participantNum = behavioralData$participant_ID # Change name for merge
+behavioralData <- behavioralData[c("participantNum", # Create smaller dataframe with only relevant variables
+                         "Mean_SCRS_Baseline", "Mean_SCRS_Control", "Mean_SCRS_Stress", 
+                         "Mean_VAS_NA_Baseline", "Mean_VAS_PAA_Baseline", "Mean_VAS_PSA_Baseline", "VAS_Stress_Baseline", 
+                         "Mean_VAS_NA_Control_Post_Rest", "Mean_VAS_PAA_Control_Post_Rest", "Mean_VAS_PSA_Control_Post_Rest", "VAS_Stress_Control_Post_Rest",
+                         "Mean_VAS_NA_Control", "Mean_VAS_PAA_Control", "Mean_VAS_PSA_Control", "VAS_Stress_Control",
+                         "Mean_VAS_NA_Stress_Post_Rest", "Mean_VAS_PAA_Stress_Post_Rest", "Mean_VAS_PSA_Stress_Post_Rest", "VAS_Stress_Stress_Post_Rest", 
+                         "Mean_VAS_NA_Stress", "Mean_VAS_PAA_Stress", "Mean_VAS_PSA_Stress", "VAS_Stress_Stress",
+                         "taskType")]
 
-audioData <- audioData %>%
-  transform(participantNum = as.factor(participantNum),
-            taskType = as.factor(taskType),
-            descriptionType = as.factor(descriptionType),
-            experimentPhase = as.factor(experimentPhase),
-            Sex = as.factor(Sex))
+behavioralDataLong <- reshape(behavioralData, direction='long', # Transform dataframe to long format
+                  varying=list(c("Mean_VAS_NA_Baseline", "Mean_VAS_NA_Control_Post_Rest", "Mean_VAS_NA_Control", "Mean_VAS_NA_Stress_Post_Rest", "Mean_VAS_NA_Stress"),
+                               c("Mean_VAS_PAA_Baseline", "Mean_VAS_PAA_Control_Post_Rest", "Mean_VAS_PAA_Control", "Mean_VAS_PAA_Stress_Post_Rest", "Mean_VAS_PAA_Stress"),
+                               c("Mean_VAS_PSA_Baseline", "Mean_VAS_PSA_Control_Post_Rest", "Mean_VAS_PSA_Control", "Mean_VAS_PSA_Stress_Post_Rest", "Mean_VAS_PSA_Stress"),
+                               c("VAS_Stress_Baseline", "VAS_Stress_Control_Post_Rest", "VAS_Stress_Control", "VAS_Stress_Stress_Post_Rest", "VAS_Stress_Stress")),
+                  timevar='fileNum',
+                  times=c('baseline', 'control rest', 'control', 'stress rest', 'stress'),
+                  v.names=c('VAS_NA', 'VAS_PAA', 'VAS_PSA', 'VAS_Stress'),
+                  idvar= c('participantNum', 'taskType'))
 
-# This is easier to work with to plot over time
+audioData = merge(audioData, questionData, by = "participantNum") # Merge audioData with trait questionnaires
+
+# This is easier to work with to plot over time and needed to merge with behavioralData
 audioData$fileNum[audioData$fileName == "audio_picture_baseline.wav"] = "baseline"
 audioData$fileNum[audioData$fileName == "audio_referential_control.wav"] = "control"
 audioData$fileNum[audioData$fileName == "audio_picture_control.wav"] = "control rest"
 audioData$fileNum[audioData$fileName == "audio_referential_stress.wav"] = "stress"
 audioData$fileNum[audioData$fileName == "audio_picture_stress.wav"] = "stress rest"
-audioData$fileNum <- ordered(audioData$fileNum, levels = c('baseline', 'control', 'control rest', 'stress', 'stress rest'))
+
+audioData = merge(audioData, behavioralDataLong, by = c("participantNum","taskType", "fileNum"))
+
+audioData <- audioData %>% # Factorize relevant variables
+  transform(participantNum = as.factor(participantNum),
+            taskType = as.factor(taskType),
+            descriptionType = as.factor(descriptionType),
+            experimentPhase = as.factor(experimentPhase),
+            Sex = as.factor(Sex))
+audioData$fileNum <- ordered(audioData$fileNum, levels = c('baseline', 'control', 'control rest', 'stress', 'stress rest')) # Factorize (ordered) moment
+
 summary(audioData)
 
 ####### Speech features #######
